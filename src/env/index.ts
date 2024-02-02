c7d01ea4de2fa81ea470cd6c6a97ed6b977a1be3
Diogo Silva <diogomfc@hotmail.com>
2024-02-02 17:27:53 -0300

    feat:(config) Vercel file

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import { logger } from '@/utils/Logger';
+import "dotenv/config";
+import { logger } from "@/utils/Logger";
 
-import { z } from 'zod';
+import { z } from "zod";
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
     JWT_SECRET: z.string(),
     JWT_EXPIRES_IN: z.string(),
     //CORS_ORIGIN: z.string().url(),
@@ -15,14 +15,22 @@ const envSchema = z.object({
     AWS_DEFAULT_REGION: z.string(),
     S3_BASE_URL: z.string().url(),
     S3_BUCKET_NAME: z.string(),
+    //Database Vercel
+    POSTGRES_URL: z.string().url(),
+    POSTGRES_PRISMA_URL: z.string().url(),
+    POSTGRES_URL_NON_POOLING: z.string().url(),
+    POSTGRES_USER: z.string(),
+    POSTGRES_HOST: z.string().url(),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_DATABASE: z.string(),
 });
 
 export const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-    logger.error(' ❌ Variáveis de ambiente inválidas', _env.error.format());
+    logger.error(" ❌ Variáveis de ambiente inválidas", _env.error.format());
 
-    throw new Error('Variáveis de ambiente inválidas');
+    throw new Error("Variáveis de ambiente inválidas");
 }
 
 export const env = _env.data;
